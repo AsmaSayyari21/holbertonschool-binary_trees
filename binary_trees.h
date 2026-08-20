6ae4dcd921ff7820431b62f361435367de51a8b0
“AsmaSayyati21” <asmaalsayari@gmail.com>
2026-08-20 17:15:42 +0000

    header

--- binary_trees.h
+++ binary_trees.h
@@ -49,5 +49,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-
+int is_perfect_recursive(const binary_tree_t *tree, size_t depth, size_t level);
 #endif /* BINARY_TREES_H */
